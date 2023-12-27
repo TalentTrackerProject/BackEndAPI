7c698fbecfd5f5cd394cc4b4a7f621ed4d7fe1be
unknown <nizar.elhilalii@gmail.com>
2023-12-27 19:48:38 +0100

    Final Commit

--- src/test/java/com/indexation/cv/SuiteTests.java
+++
@@ -1,9 +0,0 @@
-package com.indexation.cv;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ CvUtilTest.class, CvServiceTest.class, CVWebIntegrationTest.class, TagsServiceTest.class })
-public class SuiteTests {
-}
